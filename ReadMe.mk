0829007c679f969376f4a316c7e52dde0b262cdf
PaulHermans-Baldwin <91474706+PaulHermans-Baldwin@users.noreply.github.com>
2022-03-22 17:06:51 -0400

    Create ReadMe.mk

---
+++ ReadMe.mk
@@ -0,0 +1,17 @@
+####Instructions
+Your task is to write a text-based wordle game. This means that you can not use color to represent all the letter.
+Instead, you will have to figure out how to display "green" and "yellow" letters using just text.
+
+#### Rules of Game
+* User has up to 6 turns to guess the secret word.
+* If their guess is not correct, then it will be encoded as follows:
+** letters in the correct place are green.
+** letters in the wrong place but that are in the word are yellow.
+** letters not in the word are gray.
+
+* User can only guess words that are in the dictionary.
+* The dictionary will contain only words that are 5-letters long.
+
+#### Notes
+* To simplify the code, you should make a dictionary with 10 5-letter words in it.
+* Pay attention to user interface! Make your game presentable and playable by any random student who passes by our room.
